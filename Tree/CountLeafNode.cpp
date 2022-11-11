7c719bc3a2af4e1dfa6bb6cb35173b3feb322f0e
MoneyBatish <money0465.be20@chitkara.edu.in>
2022-11-12 01:34:01 +0530

    Count Leaf Node By InOrder Traversal

--- Tree/CountLeafNode.cpp
+++ Tree/CountLeafNode.cpp
@@ -18,3 +18,23 @@ int noOfLeafNodes(BinaryTreeNode<int> *root){
     PreOrder(root,count);
     return count;
 }
+////////////INORDER TRAVERSAL//////////////////
+void InOrder(BinaryTreeNode<int>* root,int &count)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    InOrder(root->left,count);
+    if(root->left==NULL && root->right==NULL)
+    {
+        count++;
+    }
+    InOrder(root->right,count); 
+}
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    // Write your code here.
+    int count=0;
+    InOrder(root,count);
+    return count;
+}
