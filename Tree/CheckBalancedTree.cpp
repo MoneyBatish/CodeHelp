f884bbe70f8536fcf177e8627d67460645cbf9f8
MoneyBatish <money0465.be20@chitkara.edu.in>
2022-11-14 01:09:25 +0530

    Check For Balanced Tree Approach #1

---
+++ Tree/CheckBalancedTree.cpp
@@ -0,0 +1,36 @@
+///////////APPROACH #1/////////////////
+int height(Node *root)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        int left=height(root->left);
+        int right=height(root->right);
+        
+        int ans=max(left,right)+1;
+        return ans;
+    }
+    //Function to check whether a binary tree is balanced or not.
+    bool isBalanced(Node *root)
+    {
+        //  Your Code here
+        if(root==NULL)
+        {
+            return true;
+        }
+        
+        bool left=isBalanced(root->left);
+        bool right=isBalanced(root->right);
+        
+        bool diff=abs(height(root->left) - height(root->right)) <=1;
+        
+        if(left && right && diff)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
