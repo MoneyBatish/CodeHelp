801d30bbe2f9bf65666c7bc2b908664131087163
MoneyBatish <money0465.be20@chitkara.edu.in>
2022-11-18 02:41:09 +0530

    Left View Of Binary Tree

--- Tree/LeftViewOfBinaryTree.cpp
+++ Tree/LeftViewOfBinaryTree.cpp
@@ -9,10 +9,11 @@ void solve(Node * root,vector<int>&ans,int level)
     {
         ans.push_back(root->data);
     }
-    solve(root->right,ans,level+1);
+    
     solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
 }
-vector<int> RightView(Node *root)
+vector<int> leftView(Node *root)
 {
    // Your code here
    vector<int>ans;
