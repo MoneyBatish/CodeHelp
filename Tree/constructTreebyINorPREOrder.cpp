6daddd856a24612f7e2930489d42c0ca68757030
MoneyBatish <money0465.be20@chitkara.edu.in>
2022-11-30 02:57:05 +0530

    Construct Tree From In Order And pre Order By Using Map

--- Tree/constructTreebyINorPREOrder.cpp
+++ Tree/constructTreebyINorPREOrder.cpp
@@ -32,4 +32,38 @@ int find(int in[],int ele,int n)
         Node *ans=solve(in,pre,preIndex,0,n-1,n);
         return ans;
     }
-    
+
+///////////#2 Approach////////////
+
+int createmap(int in[],map<int,int>&map,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            map[in[i]]=i;
+        }
+    }
+    Node *solve(int in[],int pre[],int &index,int inStart,int inEnd,int n,map<int,int>&map)
+    {
+        if(index>n || inStart>inEnd)
+        {
+            return NULL;
+        }
+        
+        int ele=pre[index++];
+        Node *temp=new Node(ele);
+        int position=map[ele];
+        temp->left=solve(in,pre,index,inStart,position-1,n,map);
+        temp->right=solve(in,pre,index,position+1,inEnd,n,map);
+        
+        return temp;
+        
+    }
+    Node* buildTree(int in[],int pre[], int n)
+    {
+        // Code here
+        int preIndex=0;
+        map<int,int>map;
+        createmap(in,map,n);
+        Node *ans=solve(in,pre,preIndex,0,n-1,n,map);
+        return ans;
+    }
