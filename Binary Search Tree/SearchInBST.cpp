dd039db1cd872d5dc1e5653daaf9eda80ec79b2a
MoneyBatish <money0465.be20@chitkara.edu.in>
2022-12-31 02:25:28 +0530

    Searching in BST

---
+++ Binary Search Tree/SearchInBST.cpp
@@ -0,0 +1,19 @@
+bool searchInBST(Node *root, int x) {
+    // Write your code here.
+    if(root==NULL)
+    {
+        return false;
+    }
+    if(root->data==x)
+    {
+        return true;
+    }
+    
+    if(root->data>x)
+    {
+        return searchInBST(root->left,x);
+    }
+    else{
+        return searchInBST(root->right,x);
+    }
+}
