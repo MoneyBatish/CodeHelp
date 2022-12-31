c8ef06f25f9f340af30668f11f29aff97fd838f6
MoneyBatish <money0465.be20@chitkara.edu.in>
2023-01-01 05:09:38 +0530

    Kth Largest In BST By Vector Approach

---
+++ Binary Search Tree/KthLargestInBST.cpp
@@ -0,0 +1,18 @@
+//vector approach
+void inorder(Node* root,vector<int>&ans)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    inorder(root->left,ans);
+    ans.push_back(root->data);
+    inorder(root->right,ans);
+}
+int kthLargest(Node *root, int k) {
+    // Write your code here
+    vector<int>ans;
+    inorder(root,ans);
+    
+        return ans[ans.size()-k];
+}
