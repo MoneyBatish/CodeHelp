5c80eddffd0259b3619b815ecb08eb790b97d8d9
MoneyBatish <money0465.be20@chitkara.edu.in>
2023-01-01 05:00:36 +0530

    Kth Smallest IN Binary Search tree By Iterative Approach

--- Binary Search Tree/kthSmallestInBST.cpp
+++ Binary Search Tree/kthSmallestInBST.cpp
@@ -22,3 +22,31 @@ int kthSmallest(BinaryTreeNode<int>* root, int k) {
         return ans[k-1];
     }
 }
+
+
+// Iterative Approach
+int solve(TreeNode<int>*root,int &i,int k)
+{
+    if(root==NULL)
+    {
+        return -1;
+    }
+    int left=solve(root->left,i,k);
+    if(left != -1){
+        return left;
+    }
+    i++;
+    if(i==k)
+    {
+        return root->data;
+    }
+    
+    return solve(root->right,i,k);
+}
+int kthSmallest(TreeNode<int> *root, int k)
+{
+	//	Write the code here.
+    int i=0;
+   int ans=solve(root,i,k);
+    return ans;
+}
