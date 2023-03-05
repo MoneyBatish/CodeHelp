e4b7fc0341e3ebaedd32bcbd995bc5cf6502ff30
MoneyBatish <money0465.be20@chitkara.edu.in>
2023-03-06 01:18:00 +0530

    Kth Largest Sum SubArray #approach 2

--- Heap/KthLargestSumSubArray.cpp
+++ Heap/KthLargestSumSubArray.cpp
@@ -21,3 +21,37 @@ int getKthLargest(vector<int> &arr, int k)
 	sort(ans.begin(),ans.end());
 	return ans[ans.size()-k];
 }
+
+/*
+   #approah 2
+   Time Complexity = N2
+   Space Complexity = O(k)
+*/
+
+#include<bits/stdc++.h>
+int getKthLargest(vector<int> &arr, int k)
+{
+	//	Write your code here.
+	priority_queue<int,vector<int>,greater<int>>mini;
+	int n=arr.size();
+	for(int i=0;i<n;i++)
+	{
+		int sum=0;
+		for(int j=i;j<n;j++)
+		{
+			sum+=arr[j];
+			if(mini.size()<k)
+			{
+				mini.push(sum);
+			}
+			else{
+				if(sum>mini.top())
+				{
+					mini.pop();
+					mini.push(sum);
+				}
+			}
+		}
+	}
+	return mini.top();
+}
